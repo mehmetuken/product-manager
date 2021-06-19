6beeac1a880f478dda5c953c16cf6a42494e0640
Mehmet Tüken <mehmetuken@hotmail.com>
2021-06-19 21:48:57 +0300

    create product entity

---
+++ src/ProductManager.Domain/Products/Product.cs
@@ -0,0 +1,32 @@
+using System;
+using JetBrains.Annotations;
+using ProductManager.Categories;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ProductManager.Products
+{
+    public class Product : FullAuditedAggregateRoot<Guid>
+    {
+        public string Title { get; }
+
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+        public Guid? CategoryId { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        protected Product()
+        {
+            // for serialization and deserialization 
+        }
+
+        public Product(Guid id,[NotNull] string title, int stockQuantity) : base(id)
+        {
+            Title = Check.NotNullOrEmpty(title,nameof(title));
+            StockQuantity = stockQuantity;
+        }
+    }
+}
