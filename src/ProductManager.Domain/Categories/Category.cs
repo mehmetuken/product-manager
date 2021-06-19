a92fd6e71b3e906f8fa58447ece1f8ec0e7ceade
Mehmet Tüken <mehmetuken@hotmail.com>
2021-06-19 21:48:49 +0300

    create category entity.

---
+++ src/ProductManager.Domain/Categories/Category.cs
@@ -0,0 +1,25 @@
+using System;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ProductManager.Categories
+{
+    public class Category : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public int MinStockQuantity { get; set; }
+
+        protected Category()
+        {
+            // for serialization and deserialization 
+        }
+
+        public Category(Guid id, [NotNull] string name, int minStockQuantity) : base(id)
+        {
+            Name = Check.NotNullOrEmpty(name, nameof(name));
+            MinStockQuantity = minStockQuantity;
+        }
+    }
+}
